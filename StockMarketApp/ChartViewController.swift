a5eaa6d6dd57f210814cbc4e5082eaa0eb4a4ecf
Douglas Voss <douglas.c.voss@gmail.com>
2017-11-07 20:26:44 -0700

    Uses string encoded from Postman to make the api call succeed.

--- StockMarketApp/ChartViewController.swift
+++ StockMarketApp/ChartViewController.swift
@@ -36,13 +36,38 @@ class ChartViewController: UIViewController, UISearchBarDelegate {
             simpleAlert(vc: self, title: NSLocalizedString("Error", comment: "Alert title"), message: NSLocalizedString("Could not make url", comment: "Alert message"), ackStr: NSLocalizedString("OK", comment: "Alert button"))
             return;
         }
-        var request = URLRequest(url: url);
-        request.httpMethod = "GET"
+        //var request = URLRequest(url: url);
+        //request.httpMethod = "GET"
         
         //dismiss keyboard
         self.view.endEditing(true)
         
-        let task = URLSession.shared.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) -> Void in
+        // http%3A%2F%2Fdev.markitondemand.com%2FApi%2Fv2%2FInteractiveChart%2Fjson%3F
+        
+        //parameters=%7B%22Normalized%22%3Afalse,%22NumberOfDays%22%3A365,%22DataPeriod%22%3A%22Day%22,%22Elements%22%3A%5B%7B%22Symbol%22%3A%22LLL%22,%22Type%22%3A%22price%22,%22Params%22%3A%5B%22c%22%5D%7D%5D%7D, NSErrorFailingURLKey=http%3A%2F%2Fdev.markitondemand.com%2FApi%2Fv2%2FInteractiveChart%2Fjson%3Fparameters=%7B%22Normalized%22%3Afalse,%22NumberOfDays%22%3A365,%22DataPeriod%22%3A%22Day%22,%22Elements%22%3A%5B%7B%22Symbol%22%3A%22LLL%22,%22Type%22%3A%22price%22,%22Params%22%3A%5B%22c%22%5D%7D%5D%7D
+        
+//        let headers = [
+//            "cache-control": "no-cache",
+//            "postman-token": "6921a2aa-319b-c544-297c-314d5e93dc7c"
+//        ]
+//
+        var request = NSMutableURLRequest(url: URL(string: "http://dev.markitondemand.com/Api/v2/InteractiveChart/json?parameters=%7B%22Normalized%22%3Afalse%2C%22NumberOfDays%22%3A365%2C%22DataPeriod%22%3A%22Day%22%2C%22Elements%22%3A%5B%7B%22Symbol%22%3A%22LLL%22%2C%22Type%22%3A%22price%22%2C%22Params%22%3A%5B%22c%22%5D%7D%5D%7D")!)
+        request.httpMethod = "GET"
+//        request.allHTTPHeaderFields = headers
+//
+//        let session = NSURLSession.sharedSession()
+//        let dataTask = session.dataTaskWithRequest(request, completionHandler: { (data, response, error) -> Void in
+//            if (error != nil) {
+//                println(error)
+//            } else {
+//                let httpResponse = response as? NSHTTPURLResponse
+//                println(httpResponse)
+//            }
+//        })
+//
+//        dataTask.resume()
+        
+        let task = URLSession.shared.dataTask(with: request as! URLRequest) { (data: Data?, response: URLResponse?, error: Error?) -> Void in
             
             DispatchQueue.main.async( execute: {
                 
